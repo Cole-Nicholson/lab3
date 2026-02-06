324a4420d4dfa470a47b723e37bcce9f6245b0ac
Zoe Lee <zlee08@calpoly.edu>
2026-02-05 20:56:56 -0800

    range/occurs tests

--- functions.py
+++ functions.py
@@ -12,11 +12,24 @@ class LLNode:
     value: int
     rest: LinkedList
 
+#return a linked list of numbers from 0 to a max num
 def range(max_exclusive: int) -> LinkedList:
-    pass
+    #helper to build range
+    def helper(current: int) -> LinkedList:
+        if current == max_exclusive:
+            return None
+        return LLNode(current, helper(current + 1))
+    return helper(0)
 
+#returns true if a number is in linked list
 def occurs(ll:LinkedList, num: int) -> bool:
-    pass
+    match ll:
+        case None:
+            return False
+        case LLNode(value, rest):
+            if num == value:
+                return True
+            return occurs(rest, num)
 
 def has_dup(ll:LinkedList) -> int:
     pass
@@ -24,8 +37,19 @@ def has_dup(ll:LinkedList) -> int:
 def insertion_sort(ll:LinkedList) -> LinkedList:
     pass
 
+linked1 = LLNode( 10, LLNode(11, LLNode(12, LLNode(13, None))))
 class Tests(unittest.TestCase):
-    pass
+    def test_range(self):
+        self.assertEqual(range(4), LLNode(0,LLNode(1,LLNode(2,LLNode(3,None)))))
+        self.assertEqual(range(0), None)
+        self.assertEqual(range(1), LLNode(0,None))
+        self.assertEqual(range(7), LLNode(0,LLNode(1,LLNode(2,LLNode(3,LLNode(4, LLNode(5, LLNode(6, None))))))))
+
+    def test_occurs(self):
+        self.assertEqual(occurs(linked1, 10), True)
+        self.assertEqual(occurs(linked1, 9), False)
+        self.assertEqual(occurs(linked1, 13), True)
+
 
 if(__name__== '__main__'):
     unittest.main()
